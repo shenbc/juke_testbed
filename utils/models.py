ef8e5472ad13f114a3de8439cb4d4979d50c5d8b
Shen <49935868+shenbc@users.noreply.github.com>
2022-05-18 23:22:57 +0800

    add lstm model

--- utils/models.py
+++ utils/models.py
@@ -40,6 +40,21 @@ class m_AlexNet(nn.Module):
         x = self.classifier(x)
         return F.log_softmax(x, dim=1)
 
+class LSTMnet(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_layer, n_class):
+        super(LSTMnet, self).__init__()
+        self.n_layer = n_layer
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(in_dim, hidden_dim, n_layer, batch_first=True)
+        self.linear = nn.Linear(hidden_dim, n_class)
+
+    def forward(self, x):  # x‘s shape (batch_size, 序列长度, 序列中每个数据的长度)
+        out, _ = self.lstm(x)  # out‘s shape (batch_size, 序列长度, hidden_dim)
+        out = out[:, -1, :]  # 中间的序列长度取-1，表示取序列中的最后一个数据，这个数据长度为hidden_dim，
+        # 得到的out的shape为(batch_size, hidden_dim)
+        out = self.linear(out)  # 经过线性层后，out的shape为(batch_size, n_class)
+        return out
+
 
 def get_model(model_name, download=False):
     """
@@ -52,10 +67,16 @@ def get_model(model_name, download=False):
         model = m.squeezenet1_0(num_classes=10)
     elif model_name == 'vgg16':
         model = m.vgg16(num_classes=10)
+    elif model_name == 'vgg19':
+        model = m.vgg19(num_classes=10)
     elif model_name == 'alexnet':
         model = m_AlexNet()
     elif model_name == 'resnet18':
         model = m.resnet18(num_classes=10)
+    elif model_name == 'resnet50':
+        model = m.resnet50(num_classes=10)
+    elif model_name == 'lstm':
+        model = LSTMnet()
 
 
     if model is not None:
